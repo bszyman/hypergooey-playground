59819606a90480a81d5a94498a66b5f852d02b64
Ben Szymanski <bszyman@icloud.com>
2026-03-05 03:31:05 -0600

    Project import

--- HyperGooey Playground/AppDelegate.m
+++ HyperGooey Playground/AppDelegate.m
@@ -6,16 +6,35 @@
 //
 
 #import "AppDelegate.h"
+#import <HyperGooey/HyperGooey.h>
 
 @interface AppDelegate ()
 
 @property (strong) IBOutlet NSWindow *window;
+
+@property (strong) HyperGooeyViewController *gooeyViewController;
+
 @end
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Urls to try:
+    // * SourceList - https://workspaces.fastgooey.com/hypermedia/fymxP-Ad8UqAx6OpuEIL7w
+    // * Outline - https://workspaces.fastgooey.com/hypermedia/5yha7HvKYU-85x_kbq0KRQ
+    // * Collection -
+    //   * https://workspaces.fastgooey.com/hypermedia/KOX7EPdq0ky5Qkp5wPg42A
+    //   * https://workspaces.fastgooey.com/hypermedia/4RY89bglr0i78hSVJv94TQ
+    //   * https://workspaces.fastgooey.com/hypermedia/lkqRmQ0wdU-67bEAZemWSg
+    //   * https://workspaces.fastgooey.com/hypermedia/FKZJTULLkUe2uWAtO3GbZg
+    //   * https://workspaces.fastgooey.com/hypermedia/aVq9ANgK40u0qVpIyXq_lA
+    // * Table - https://workspaces.fastgooey.com/hypermedia/T5HkX9CaG0KwXS_7vidBlA
+    // * Content - https://workspaces.fastgooey.com/hypermedia/35ynSn4JZk2VjZm_rYVJBA
+    
+    self.gooeyViewController = [[HyperGooeyViewController alloc] initWithUrl:@"https://workspaces.fastgooey.com/hypermedia/5yha7HvKYU-85x_kbq0KRQ"];
+    [self.gooeyViewController mountInContainer:self.window.contentView
+                                        parent:self.window.contentViewController];
 }
 
 
