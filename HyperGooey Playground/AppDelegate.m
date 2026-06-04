1b2648dbed7780d0ea0ceace2bbce93069813d2e
Ben Szymanski <bszyman@icloud.com>
2026-06-03 19:56:08 -0500

    Implementing opening interfaces by url using the user interface

--- HyperGooey Playground/AppDelegate.m
+++ HyperGooey Playground/AppDelegate.m
@@ -14,6 +14,11 @@
 
 @property (strong) HyperGooeyViewController *gooeyViewController;
 
+@property (weak) IBOutlet NSWindow *openInterfaceWindow;
+@property (weak) IBOutlet NSTextField *interfaceUrlTextField;
+
+@property (copy) NSString *currentIntefaceUrl;
+
 @end
 
 @implementation AppDelegate
@@ -32,7 +37,9 @@
     // * Table - https://workspaces.fastgooey.com/hypermedia/T5HkX9CaG0KwXS_7vidBlA
     // * Content - https://workspaces.fastgooey.com/hypermedia/35ynSn4JZk2VjZm_rYVJBA
     
-    self.gooeyViewController = [[HyperGooeyViewController alloc] initWithUrl:@"https://workspaces.fastgooey.com/hypermedia/5yha7HvKYU-85x_kbq0KRQ"];
+    self.currentIntefaceUrl = [NSMutableString stringWithString:@"https://demo.hyperfastinteractive.com/"];
+    
+    self.gooeyViewController = [[HyperGooeyViewController alloc] initWithUrl:self.currentIntefaceUrl];
     [self.gooeyViewController mountInContainer:self.window.contentView
                                         parent:self.window.contentViewController];
 }
@@ -45,6 +52,54 @@
 
 - (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
     return YES;
+}
+
+-(IBAction)cancelOpenInterface:(id)sender
+{
+    [self.window endSheet:self.openInterfaceWindow
+               returnCode:NSModalResponseCancel];
+}
+
+- (IBAction)navigateToInterface:(id)sender
+{
+    if (self.openInterfaceWindow.sheetParent != nil) {
+        [self.window endSheet:self.openInterfaceWindow
+                   returnCode:NSModalResponseOK];
+        return;
+    }
+
+    [self refreshInterface:nil];
+}
+
+- (IBAction)presentOpenInterfaceSheet:(id)sender
+{
+    self.interfaceUrlTextField.stringValue = self.currentIntefaceUrl ?: @"";
+
+    [self.window beginSheet:self.openInterfaceWindow
+          completionHandler:^(NSModalResponse response) {
+        if (response != NSModalResponseOK) {
+            return;
+        }
+
+        self.currentIntefaceUrl = self.interfaceUrlTextField.stringValue;
+        [self refreshInterface:nil];
+    }];
+}
+
+- (IBAction)refreshInterface:(id)sender
+{
+    NSViewController *containerViewController = self.window.contentViewController;
+    for (NSViewController *childViewController in [containerViewController.childViewControllers copy]) {
+        [childViewController removeFromParentViewController];
+    }
+
+    for (NSView *subview in [self.window.contentView.subviews copy]) {
+        [subview removeFromSuperview];
+    }
+
+    self.gooeyViewController = [[HyperGooeyViewController alloc] initWithUrl:self.currentIntefaceUrl];
+    [self.gooeyViewController mountInContainer:self.window.contentView
+                                        parent:self.window.contentViewController];
 }
 
 
